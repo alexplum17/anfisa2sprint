d0db76b21cd68f7f14640f241bd27549cf676bb6
Alex Perov <aleksandrperov97@yandex.ru>
2024-12-08 13:07:21 +0300

    Много всяких изменений

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -57,6 +57,10 @@ class IceCream(PublishedModel):
         blank=True,
         verbose_name='Обёртка',
     )
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
@@ -65,10 +69,13 @@ class IceCream(PublishedModel):
     )
     toppings = models.ManyToManyField(Topping, verbose_name='Топпинги')
     is_on_main = models.BooleanField(default=False, verbose_name='На главную')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
 
     class Meta:
         verbose_name = 'Мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
